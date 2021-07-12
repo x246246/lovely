a5e12b30cf3d156ac5a150f6be4a3c03745fe2f5
x246246 <64516771+x246246@users.noreply.github.com>
2021-07-12 14:31:52 +0300

    Update json.js

--- json.js
+++ json.js
@@ -1,6 +1,6 @@
 $().ready(function(){
     $.getJSON( "https://api.eva.pingutil.com/email?email=test@mail7.io", function( data ) {
     console.log(data);
-    $("#text").html(data["status"]);
+    $("#text").html(data["data"]);
   });
 });
