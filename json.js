1d8926577fe4d46d6591e07057cc33f9c5c4b477
x246246 <64516771+x246246@users.noreply.github.com>
2021-07-12 14:19:28 +0300

    Update json.js

--- json.js
+++ json.js
@@ -1,5 +1,5 @@
 $().ready(function(){
-    $.getJSON( "/data.json", function( data ) {
+    $.getJSON( "/lovely/data.json", function( data ) {
     console.log(data);
     $("#text").html(data["text"]);
   });
